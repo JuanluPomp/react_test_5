2746041b040813fecf8c2c38d1e036689ac28e13
Juan Luis <juanluis@gmail.com>
2025-05-18 17:33:47 -0700

    initialzed

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className=' container mx-auto'>
+        <h1 className=' text-3xl font-bold text-center mt-10'>Shoping Cart</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
